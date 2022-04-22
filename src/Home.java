b8cb26a687e37f70d43928df7281aa2ec70d4f70
LAPTOP-TODA2RK8\Geo <georgiadisgiorgos4268@gmail.com>
2022-04-22 13:36:34 +0300

    fixinig home

--- src/Home.java
+++ src/Home.java
@@ -19,100 +19,10 @@ public class Home {
 	}
 	
 	
-	
-	public double getRent() {
-		return rent;
+
+	public Home gethome() {
+		return this;
 	}
-
-
-
-	public void setRent(double rent) {
-		this.rent = rent;
-	}
-
-
-	public String getAddress() {
-		return address;
-	}
-
-
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-
-	public String getArea() {
-		return area;
-	}
-
-
-
-	public void setArea(String area) {
-		this.area = area;
-	}
-
-
-
-	public boolean getPetsAllowd() {
-		return petsAllowed;
-	}
-
-
-
-	public void setPetsAllowd(boolean petsAllowd) {
-		this.petsAllowed = petsAllowed;
-	}
-
-
-
-	public String getTypeOfHeating() {
-		return typeOfHeating;
-	}
-
-
-
-	public void setTypeOfHeating(String typeOfHeating) {
-		this.typeOfHeating = typeOfHeating;
-	}
-
-
-
-	public int getBedrooms() {
-		return bedrooms;
-	}
-
-
-
-	public void setBedrooms(int bedrooms) {
-		this.bedrooms = bedrooms;
-	}
-
-
-
-	public int getFloor() {
-		return floor;
-	}
-
-
-
-	public void setFloor(int floor) {
-		this.floor = floor;
-	}
-
-
-
-	public int getCm2() {
-		return m2;
-	}
-
-
-
-	public void setCm2(int m2) {
-		this.m2 = m2;
-	}
-
 	
 	
 
